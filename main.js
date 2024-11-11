66ae923000201390678e08ef5c014999bbfca64e
nasuda <nasuda908@gmail.com>
2024-11-11 10:23:56 +0900

    add Formula

--- main.js
+++ main.js
@@ -2,9 +2,22 @@ console.log("main");
 
 const args = process.argv;
 
-console.log(args)
+//console.log(args)
 
 
 const x = args[2];
 console.log(x);
 
+
+const y = args[3];
+
+
+
+const z = x / (y + x);
+
+//console.log(z + "%");
+console.log(z)
+
+
+const parcentage = z.toFixed(3);
+console.log(parcentage * 100 + "%");
